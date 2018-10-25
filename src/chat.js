c7cbfd0ca823b76eb91668b4c2f551a24be31a97
BaohuiXie <bxie056@uottawa.ca>
2018-10-25 17:35:13 -0400

    chat_update

--- src/chat.js
+++ src/chat.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import imag from './picture/picture.png';
 import * as firebase from 'firebase';
-import {storage} from './index';
+import { storage } from './index';
 import "./chat.css";
+const uuidv4 = require('uuid/v4');
+// import uuidv4 from 'uuid/v4';
 
 export default class Chat extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             text: '',
-            textarray: [],
-            url:[],
+            message: [],
+            url: '',
         };
-        this.listenToMESSAGE();
+        this.listeningMessage()
+        // this.listenToImage();
     }
 
     onChangeText(event) {
@@ -23,110 +26,90 @@ export default class Chat extends React.Component {
         );
     }
 
-    sendText = (event) => {
-        writeTESTtoFIREBASE(this.state.text)
-        this.state.text && this.setState({text:''})
+
+    sendText = () => {
+        firebase.database().ref('message').push({
+            content: this.state.text,
+            type: 'text'
+        })
+        this.state.text && this.setState({ text: '' })
     }
 
 
-    listenToMESSAGE() {
+    listeningMessage() {
         this.text_array = [];
         var messageLisener = firebase.database().ref('messages');
-        messageLisener.on('child_added', (data) => {
-            // console.log(data.key);
-            // console.log(data.val().text);
-            const val = data.val();
-
-            //if(val = )
-            this.text_array.push(data.val().text)
-            this.setState({textarray: this.text_array});
+        messageLisener.on('child_added', snapshot => {
+            const val = snapshot.val();
+            this.setState({ message: [...this.state.message, val] })
         })
     }
 
-    handleChange(event){
-        writeIMAGEtoFIREBASE( event.target.files[0])
-        this.state.selectedImage && this.setState({selectedImage:null})
-    }
+    sendImage = (evt) => {
+        const selectedImage = evt.nativeEvent.target.files[0];
+        const random = uuidv4();
+        const img_ref = firebase.storage().ref('images/' + random)
+        const upload_task = img_ref.put(selectedImage);
 
-    sendImage = () => {
-        writeIMAGEtoFIREBASE(this.state.selectedImage)
-        this.state.selectedImage && this.setState({selectedImage:null})
-    }
-
-    listenToImage=()=>{
-        this.url_array = [];
-        var storageRef = storage.ref();
-        var imagesRef = storageRef.child('images/abc.jpg');
-        imagesRef.getDownloadURL().then(function(url) {
-            this.url_array.push(url)
-            this.setState({url: this.url_array});
+        upload_task.on(firebase.storage.TaskEvent.STATE_CHANGED, null, null, () => {
+            img_ref.getDownloadURL().then(url => {
+                firebase.database().ref('message').push({
+                    content: url,
+                    type: 'image'
+                })
+            })
         })
-    }
-    scrollToBottom(){
-        this.message.scrollToBottom({behavior:'smooth'});
+        this.state.selectedImage && this.setState({ selectedImage: null })
     }
 
-// change->send->listen
+
+    scrollToBottom() {
+        this.message.scrollToBottom({ behavior: 'smooth' });
+    }
+
+    // change->send->listen
     render() {
+        const { message } = this.state;
         return (
             <div>
                 <h1 id='title'>Chat</h1><hr />
                 <div id='Block'>
                     <div id='messageBlock'>
-                        <div ref={message=>this.message=message}>
-                            {this.state.textarray.map((val, index) => [<p key={index}>{val}</p>, <br key={index} />])}
-                            {this.state.url.map((val2,index)=>[<img key={index} src={val2}/>])}
+                        <div ref={message => this.message = message}>
+Zx                            {
+                                message.map((msg, index) => {
+                                    const { type, content } = msg;
+                                    switch (type) {
+                                        case 'text':
+                                            return (
+                                                <div style={{margin:20}}>
+                                                    <h4 key={index + 'name'}> {(index % 2 === 0) ? 'Dave' : 'Paula'}</h4 >
+                                                    <p key={index + 'msg'}>{content}</p> 
+                                                </div>
+                                            )
+                                        case 'image':
+                                            return (
+                                                <h4 key={index + 'name'}> {(index % 2 === 0) ? 'Dave' : 'Paula'}</h4 >,
+                                                <img src={content} width={100} height={100}/>
+                                            );
+                                    }
+                                })
+                            }
                         </div>
                     </div>
                     <div>
                         <input value={this.state.text} type='text' placeholder='TextLine'
-                            onChange={this.onChangeText.bind(this)}/>
+                            onChange={this.onChangeText.bind(this)} />
                         <button className='button' onClick={this.sendText} >Send</button>
-                        <img src={imag} onClick={ ()=>this.fileInput.click()} height='30px' width='30px'/>
-                        <input type='file' 
-                        onChange={this.handleChange.bind(this)} 
-                        style={{display: 'none'}} 
-                        ref={fileInput =>this.fileInput=fileInput}/>
-                        {/* <button className='button' onClick={this.sendImage}>UploadImage</button>  */}
-                    </div>        
+                        <img src={imag} onClick={() => this.fileInput.click()} height='30px' width='30px' />
+                        <input type='file'
+                            onChange={this.sendImage}
+                            style={{ display: 'none' }}
+                            ref={fileInput => this.fileInput = fileInput} />
+                    </div>
                 </div>
             </div>
         );
     }
 }
 
-
-function writeTESTtoFIREBASE(text) {
-    firebase.database().ref('messages').push({
-        text: text,
-    });
-}
-
-function writeIMAGEtoFIREBASE(selectedImage){
-    firebase.storage().ref('images/abc.jpg').put(selectedImage);
-    console.log(selectedImage);
-}
-
-
-
-//   function writeNewPost(uid, username, picture, title, body) {
-//     // A post entry.
-//     var postData = {
-//       author: username,
-//       uid: uid,
-//       body: body,
-//       title: title,
-//       starCount: 0,
-//       authorPic: picture
-//     };
-
-//     // Get a key for a new Post.
-//     var newPostKey = firebase.database().ref().child('posts').push().key;
-
-//     // Write the new post's data simultaneously in the posts list and the user's post list.
-//     var updates = {};
-//     updates['/posts/' + newPostKey] = postData;
-//     updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
-//     return firebase.database().ref().update(updates);
-//   }
